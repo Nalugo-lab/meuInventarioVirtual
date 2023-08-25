84feb36b2b7c065233860918e4ad55a85200f4f7
hugobillemartins@gmail.com <hugobillemartins@gmail.com>
2023-08-24 21:46:29 -0300

    hi

--- authentication/models.py
+++ authentication/models.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.db import models
 from django.db import models
 from django.contrib.auth.models import PermissionsMixin, UserManager
@@ -6,20 +7,21 @@ from django.contrib.auth.base_user import AbstractBaseUser
 from django.utils import timezone
 
 
-USER_STATUS = (
-    (1, "Account created"),
-    (2, "Active account"),
-    (3, "Active"),
-    (4, "Disabled"),
-)
+
 
 
 class User(AbstractBaseUser, PermissionsMixin):
+    USER_STATUS = [
+        (1, "Account created"),
+        (2, "Active account"),
+        (3, "Active"),
+        (4, "Disabled")
+    ]
 
     name = models.CharField(("name"), max_length=150, blank=True)
     surname = models.CharField(("surname"), max_length=150, blank=True)
     email = models.EmailField(("email address"), unique=True)
-    cellphone_number = models.CharField(unique=True)
+    cellphone_number = models.CharField(unique=True, max_length=11)
     is_staff = models.BooleanField(
         ("staff status"),
         default=False,
@@ -35,8 +37,8 @@ class User(AbstractBaseUser, PermissionsMixin):
         ),
     )
     email_verified = models.BooleanField(default=False)
-    account_status = models.Choices(USER_STATUS, default=1)
-    date_joined = models.DateTimeField(("date joined"), default=timezone.now)
+    account_status = models.IntegerField(choices=USER_STATUS)
+    datetime_joined = models.DateTimeField(("date joined"), default=timezone.now)
 
     objects = UserManager()
 
@@ -65,4 +67,6 @@ class Client(models.Model):
     name = models.CharField(("name"), max_length=150, blank=True)
     surname = models.CharField(("surname"), max_length=150, blank=True)
     email = models.EmailField(("email address"), unique=True)
-    cellphone_number = models.CharField(unique=True)
+    cellphone_number = models.CharField(unique=True, max_length=11)
+    datetime_joined = models.DateTimeField(("date joined"), default=timezone.now)
+    creator = models.ForeignKey(settings.AUTH_USER_MODEL, verbose_name=("Criador"), on_delete=models.PROTECT)
--- inventory/models.py
+++ inventory/models.py
@@ -1,7 +1,53 @@
+from django.conf import settings
 from django.db import models
+from django.utils import timezone
+from authentication.models import Client
+
 
 # Create your models here.
+class Tag(models.Model):
+    title = models.CharField(max_length=128)
+    creator = models.ForeignKey(settings.AUTH_USER_MODEL, verbose_name=("Criador"), on_delete=models.PROTECT)
+    creation_datetime = models.DateTimeField(verbose_name=("date joined"), default=timezone.now)
 
-class Teste(models.Model):
 
-    name = models.CharField(max_length=25)
+class Inventory(models.Model):
+
+    title = models.CharField(max_length=64)
+    creator = models.ForeignKey(settings.AUTH_USER_MODEL, verbose_name=("Criador"), on_delete=models.PROTECT)
+    creation_datetime = models.DateTimeField(verbose_name=("date joined"), default=timezone.now)
+
+
+class Product(models.Model):
+    title = models.CharField(max_length=128)
+    description = models.CharField(max_length=512)
+    quantity = models.IntegerField()
+    barcode=models.CharField(max_length=32)
+    base_price=models.FloatField()
+    commercial_price= models.FloatField()
+    tag=models.ForeignKey(Tag, on_delete=models.PROTECT)
+    creator = models.ForeignKey(settings.AUTH_USER_MODEL, verbose_name=("Criador"), on_delete=models.PROTECT)
+    creation_datetime = models.DateTimeField(verbose_name=("date joined"), default=timezone.now)
+
+
+class Transaction(models.Model):
+    datetime = models.DateTimeField(default=timezone.now)
+    client = models.ForeignKey(Client, on_delete=models.PROTECT)
+
+
+class Historic(models.Model):
+    HISTORIC_STATUS = [
+    (1, "Remove - transaction"),
+    (2, "Remove - expiration date"),
+    (3, "Remove - manual"),
+    (4, "Add"),
+    (5, "Devolution")
+    ]
+
+    datetime = models.DateTimeField(default=timezone.now)
+    quantity = models.IntegerField()
+    creator = models.ForeignKey(settings.AUTH_USER_MODEL, verbose_name=("Criador"), on_delete=models.PROTECT)
+    transaction = models.ForeignKey(Transaction, on_delete=models.PROTECT)
+    product = models.ForeignKey(Product, on_delete=models.PROTECT)
+    value = models.FloatField()
+    status = models.IntegerField(choices=HISTORIC_STATUS)
