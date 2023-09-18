36d30aa8abfe93c015ae8e6725c34e624713a943
hugobillemartins@gmail.com <hugobillemartins@gmail.com>
2023-09-18 20:43:28 -0300

    yeah

--- authentication/models.py
+++ authentication/models.py
@@ -7,6 +7,39 @@ from django.contrib.auth.base_user import AbstractBaseUser
 from django.utils import timezone
 
 
+from django.db import models
+from django.contrib.auth.base_user import BaseUserManager
+
+
+class UserManager(BaseUserManager):
+    use_in_migrations = True
+
+    def _create_user(self, email, password, **extra_fields):
+        if not email:
+            raise ValueError('Users require an email field')
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_user(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', False)
+        extra_fields.setdefault('is_superuser', False)
+        return self._create_user(email, password, **extra_fields)
+
+    def create_superuser(self, email, password, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+
+        return self._create_user(email, password, **extra_fields)
+
+
 class User(AbstractBaseUser, PermissionsMixin):
     USER_STATUS = [
         (1, "Account created"),
@@ -15,11 +48,11 @@ class User(AbstractBaseUser, PermissionsMixin):
         (4, "Disabled")
     ]
 
-    username = models.CharField(unique=True, max_length=64)
+    username = None
     name = models.CharField(("name"), max_length=150, blank=True)
     surname = models.CharField(("surname"), max_length=150, blank=True)
     email = models.EmailField(("email address"), unique=True)
-    cellphone_number = models.CharField(unique=True, max_length=11)
+    cellphone_number = models.CharField(max_length=11)
     is_staff = models.BooleanField(
         ("staff status"),
         default=False,
@@ -41,7 +74,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     objects = UserManager()
 
     EMAIL_FIELD = "email"
-    USERNAME_FIELD = 'username'
+    USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
 
     class Meta:
